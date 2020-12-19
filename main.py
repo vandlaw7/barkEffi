1917b8d2d10c0f0b1810ab1a9d4d31c71fd9019e
남창우 <changwoo@namchang-uui-MacBookPro.local>
2020-12-20 08:51:15 +0900

    second

--- main.py
+++ main.py
@@ -43,8 +43,8 @@ data_transforms = {
     ]),
     'val': transforms.Compose([
         transforms.Resize((200, 200)),
-        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
         transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
     ])
 }
 
