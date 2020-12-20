39d871345bc55fd081371036c534e9c345660092
남창우 <changwoo@namchang-uui-MacBookPro.local>
2020-12-21 00:48:22 +0900

    second

--- inference.py
+++ inference.py
@@ -38,13 +38,10 @@ def main(argv):
     return
 
 
-print(f'GREEN_ESCAPE: {GREEN_ESCAPE}, INPUT_PATH: {INPUT_PATH}')
-
 if __name__ == '__main__':
     main(sys.argv)
 
 print(f'GREEN_ESCAPE: {GREEN_ESCAPE}, INPUT_PATH: {INPUT_PATH}')
-
 
 effi_version = 0
 num_classes = 17
@@ -135,59 +132,63 @@ new_transform = transforms.Compose([
     transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
 ])
 
-test_image_dataset = data_fraction(
-    datasets.ImageFolder(INPUT_PATH, (new_transform if GREEN_ESCAPE else test_transform)))
-test_set_size = len(test_image_dataset)
-max_batch_idx = test_set_size // BATCH_SIZE
 
-test_image_loaded = DataLoader(test_image_dataset, batch_size=BATCH_SIZE,
-                               shuffle=False, num_workers=4)
+for i in range(10):
+    print(f'{i}th check')
+    test_image_dataset = data_fraction(
+        datasets.ImageFolder(INPUT_PATH, (new_transform if GREEN_ESCAPE else test_transform)))
+    test_set_size = len(test_image_dataset)
+    max_batch_idx = test_set_size // BATCH_SIZE
 
-device = torch.device("cuda:0")
-model = EfficientNet.from_pretrained(f'efficientnet-b{effi_version}', num_classes=num_classes)
-model.load_state_dict(torch.load(WEIGHTS_FILE))
-model.to(device)
+    test_image_loaded = DataLoader(test_image_dataset, batch_size=BATCH_SIZE,
+                                   shuffle=False, num_workers=4)
 
-criterion = nn.CrossEntropyLoss()
+    device = torch.device("cuda:0")
+    model = EfficientNet.from_pretrained(f'efficientnet-b{effi_version}', num_classes=num_classes)
+    model.load_state_dict(torch.load(WEIGHTS_FILE))
+    model.to(device)
 
-''' for train, not here'''
-optimizer = optim.Adam(model.parameters(), lr=0.001)
-scheduler = lr_scheduler.StepLR(optimizer, step_size=7, gamma=0.1)
+    criterion = nn.CrossEntropyLoss()
 
-
-def convert_to_preferred_format(sec):
-    sec = sec % (24 * 3600)
-    hour = sec // 3600
-    sec %= 3600
-    min = sec // 60
-    sec %= 60
-    return "%02d:%02d:%02d" % (hour, min, sec)
+    ''' for train, not here'''
+    optimizer = optim.Adam(model.parameters(), lr=0.001)
+    scheduler = lr_scheduler.StepLR(optimizer, step_size=7, gamma=0.1)
 
 
-''' inference run'''
-since = time.time()
-running_loss = 0.0
-running_corrects = 0
-for batch_idx, (inputs, labels) in enumerate(test_image_loaded):
-    inputs = inputs.to(device)
-    labels = labels.to(device)
+    def convert_to_preferred_format(sec):
+        sec = sec % (24 * 3600)
+        hour = sec // 3600
+        sec %= 3600
+        min = sec // 60
+        sec %= 60
+        return "%02d:%02d:%02d" % (hour, min, sec)
 
-    with torch.set_grad_enabled(False):
-        outputs = model(inputs)
-        _, preds = torch.max(outputs, 1)
-        loss = criterion(outputs, labels)
 
-    running_loss += loss.item() * inputs.size(0)
-    running_corrects += torch.sum(preds == labels.data)
+    ''' inference run'''
+    since = time.time()
+    running_loss = 0.0
+    running_corrects = 0
+    for batch_idx, (inputs, labels) in enumerate(test_image_loaded):
+        inputs = inputs.to(device)
+        labels = labels.to(device)
 
-    if batch_idx % check_period == 0:
-        print(f'batch_idx: {batch_idx} / {max_batch_idx}, '
-              f'time: {convert_to_preferred_format(time.time() - since)}')
+        with torch.set_grad_enabled(False):
+            outputs = model(inputs)
+            _, preds = torch.max(outputs, 1)
+            loss = criterion(outputs, labels)
 
-total_loss = running_loss / test_set_size
-total_acc = running_corrects.double() / test_set_size
+        running_loss += loss.item() * inputs.size(0)
+        running_corrects += torch.sum(preds == labels.data)
 
-f = open('./test_result.txt', 'a')
-print(f'total loss: {total_loss}, total accuracy: {total_acc}')
-f.write(f'total loss: {total_loss}, total accuracy: {total_acc}\n')
-f.close()
+        if batch_idx % check_period == 0:
+            print(f'batch_idx: {batch_idx} / {max_batch_idx}, '
+                  f'time: {convert_to_preferred_format(time.time() - since)}')
+
+    total_loss = running_loss / test_set_size
+    total_acc = running_corrects.double() / test_set_size
+
+    f = open('./test_result.txt', 'a')
+    print(f'total loss: {total_loss}, total accuracy: {total_acc}')
+    f.write(f'GREEN_ESCAPE: {GREEN_ESCAPE}, INPUT_PATH: {INPUT_PATH}')
+    f.write(f'total loss: {total_loss}, total accuracy: {total_acc}\n')
+    f.close()
