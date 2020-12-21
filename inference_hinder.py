150434eab4451ef19e35010b409aa70f49f706b3
남창우 <changwoo@namchang-uui-MacBookPro.local>
2020-12-21 21:56:28 +0900

    second

--- inference_hinder.py
+++ inference_hinder.py
@@ -130,7 +130,7 @@ def convert_to_preferred_format(sec):
 
 
 test_transform = transforms.Compose([
-    transforms.RandomCropMy(334),
+    RandomCropMy(334),
     transforms.Resize((200, 200)),
     transforms.ToTensor(),
     transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
